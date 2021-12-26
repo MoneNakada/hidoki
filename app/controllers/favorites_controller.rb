fcd1fd4b69937a41ec5c6f7411bc619540755e7c
wtmovii <scoups523@gmail.com>
2021-12-26 14:00:08 +0000

    デバッグ課題

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,20 +3,18 @@ class FavoritesController < ApplicationController
   # POST /books/:book_id/favorites
   # POST /favorites.json
   def create
-    if !Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
-      @favorite = current_user.favorites.new(:book_id => params[:book_id])
-      @favorite.save
-      redirect_to books_path
-    end
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
+    favorite.save
+    redirect_to request.referer
   end
 
 
   # DELETE /books/:book_id/favorites/
   def destroy
-    if Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
-      @favorite = Favorite.find_by(user_id: current_user, book_id: params[:book_id])
-      @favorite.destroy
-      redirect_to books_path
-    end
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
+    favorite.destroy
+    redirect_to request.referer
   end
 end
